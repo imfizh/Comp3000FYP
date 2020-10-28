eec128fffa6145f359517fb41ccf149b7a125b3f
imfizh <43548110+imfizh@users.noreply.github.com>
2020-10-28 21:42:18 +0000

    Creating unity project and setting up basic 2d movement
    this commit is just to initialize the project as well as set up basic movement in a 2d environment

---
+++ 2D Depth Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+    private Rigidbody2D rb;
+    private bool faceR = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask WIG;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WIG);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(faceR == false && moveInput > 0)
+        {
+            Flip();
+        }else if(faceR == true && moveInput <0)
+        {
+            Flip();
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        faceR = !faceR;
+        Vector3 Scalar = transform.localScale;
+        Scalar.x *= -1;
+        transform.localScale = Scalar;
+    }
+}
