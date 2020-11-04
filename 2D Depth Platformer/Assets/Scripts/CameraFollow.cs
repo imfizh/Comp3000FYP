22f189ab7bf0b599bec30a3156b18ccf8167cb10
imfizh <43548110+imfizh@users.noreply.github.com>
2020-11-04 17:55:15 +0000

    Added camera follow script
    Added a script that makes the camera follow the player as they move

---
+++ 2D Depth Platformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 offset = new Vector3(0,0,-10);
+    public float followSpeed = 10f;
+    public float xMin = 0f;
+    Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = player.position + offset;
+        //Vector3 clampedPos = new Vector3(Mathf.Clamp(targetPos.x, xMin, float.MaxValue), targetPos.y, targetPos.z);
+        Vector3 clampedPos = new Vector3(targetPos.x, targetPos.y, targetPos.z);
+        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, clampedPos, ref velocity, followSpeed * Time.deltaTime);
+        transform.position = smoothPos;
+    }
+
+    
+}
