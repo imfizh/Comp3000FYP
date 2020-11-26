80da4893a62e1ae395b32ba69524936fa24cacc5
imfizh <43548110+imfizh@users.noreply.github.com>
2020-11-26 15:40:47 +0000

    moving platform on x axis
    create scripts that allow a platform to move on the x axis, the player is also able to stand on them on get moved with it

---
+++ 2D Depth Platformer/Assets/Scripts/MoveWithPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.parent = this.transform;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.transform.parent = null;
+    }
+}
---
+++ 2D Depth Platformer/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform sPos, ePos;
+    public float speed;
+    public Transform StartPos;
+    Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = StartPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == sPos.position)
+        {
+            nextPos = ePos.position;
+        }
+        if (transform.position == ePos.position)
+        {
+            nextPos = sPos.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position,nextPos,speed*Time.deltaTime);
+    }
+}
