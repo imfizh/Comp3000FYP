2dc8cc36371f9cf495cc2388de4272b22365e10a
imfizh <43548110+imfizh@users.noreply.github.com>
2020-12-12 17:52:29 +0000

    Switch game to use lightweight render pipeline
    switch game to lightweight render pipeline, added 2d light to scene, editted alphaWall script to work with LWRP, added sorting layers to all objects currently in scene

--- 2D Depth Platformer/Assets/Scripts/AlphaWalls.cs
+++ 2D Depth Platformer/Assets/Scripts/AlphaWalls.cs
@@ -7,12 +7,13 @@ public class AlphaWalls : MonoBehaviour
     private Renderer rend;
     Color matColour;
     private bool transparent = false;
+    //These variables and the commented out code is for when not using the lightweight render pipeline
 
     // Start is called before the first frame update
     void Start()
     {
-        rend = gameObject.GetComponent<SpriteRenderer>();
-        matColour = rend.material.color;
+        //rend = gameObject.GetComponent<SpriteRenderer>();
+        //matColour = rend.material.color;
     }
 
     public void Alpha(bool transparent)
@@ -23,12 +24,14 @@ public class AlphaWalls : MonoBehaviour
 
         if (transparent)
         {
-            matColour.a = 0.5f;
+            //matColour.a = 0.5f;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0.5f);
         }
         else
         {
-            matColour.a = 1.0f;
+            //matColour.a = 1.0f;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
         }
-        rend.material.color = matColour;
+        //rend.material.color = matColour;
     }
 }
