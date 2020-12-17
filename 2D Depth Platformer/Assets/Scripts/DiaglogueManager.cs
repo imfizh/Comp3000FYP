568c798a14576079654882c6c5f53b48bd69e8cb
imfizh <43548110+imfizh@users.noreply.github.com>
2020-12-17 19:00:41 +0000

    Added dialogue system
    added scripts for dialogue system to the game, now I need to hook it up to things to call the dialogue

---
+++ 2D Depth Platformer/Assets/Scripts/DiaglogueManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiaglogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    private Queue<string> sentences;
+    public Animator animator;
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+   public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true); 
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        //dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letler in sentence.ToCharArray())
+        {
+            dialogueText.text += letler;
+            yield return null;
+        }
+    }
+    public void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+    }
+}
