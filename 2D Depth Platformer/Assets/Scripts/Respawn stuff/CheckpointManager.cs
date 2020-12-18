5242901529eb002016cfcf4ea2f32f734b1af294
imfizh <43548110+imfizh@users.noreply.github.com>
2020-12-18 18:18:34 +0000

    Respawn system completed
    now have multiple checkpoints that the player can respawn at which conform to the layer system. Checkpoints must follow naming convention of being just a number

---
+++ 2D Depth Platformer/Assets/Scripts/Respawn stuff/CheckpointManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public int lastCheckpoint = 1;
+
+    public void ChangeCheckpoint(int checkpoint)
+    {
+        lastCheckpoint = checkpoint;
+    }
+}
---
+++ 2D Depth Platformer/Assets/Scripts/Respawn stuff/Respawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player = null;
+    [SerializeField] private Transform respawn1 = null;
+    [SerializeField] private Transform respawn2 = null;
+    int currentCP = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentCP = FindObjectOfType<CheckpointManager>().lastCheckpoint;
+        if (collision.tag == "Player" & currentCP == 1)
+        {
+            if (GameObject.Find("mc").layer ==14)
+            {
+                GameObject.Find("mc").layer = 10;
+            }
+            player.transform.position = respawn1.transform.position;
+        }
+        if (collision.tag == "Player" & currentCP == 2)
+        {
+            if (GameObject.Find("mc").layer == 10)
+            {
+                GameObject.Find("mc").layer = 14;
+            }
+            player.transform.position = respawn2.transform.position;
+        }
+    }
+}
