0d30e18f99d1588aad2d8853d6833a528c539698
imfizh <43548110+imfizh@users.noreply.github.com>
2020-12-18 14:21:26 +0000

    Dialogue system completed
    added on trigger events so now the dialogue system is complete

--- 2D Depth Platformer/Assets/Scripts/DialogueTrigger.cs
+++ 2D Depth Platformer/Assets/Scripts/DialogueTrigger.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public bool isDialogue=false;
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DiaglogueManager>().StartDialogue(dialogue);
+        isDialogue = true;
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TriggerDialogue();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FindObjectOfType<DiaglogueManager>().EndDialogue();
+        isDialogue = false;
+    }
+
+    public void Update()
+    {
+        if (isDialogue == true && Input.GetKeyDown(KeyCode.Return))
+        {
+            FindObjectOfType<DiaglogueManager>().DisplayNextSentence();
+        }
+    }
+
 }
