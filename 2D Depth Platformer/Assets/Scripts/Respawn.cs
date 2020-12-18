335252a7fd127583c97dd9f29c7d76afce9905ac
imfizh <43548110+imfizh@users.noreply.github.com>
2020-12-18 16:54:49 +0000

    implemented death barrier
    added death barrier but it doesn't work if the player is on back layer

---
+++ 2D Depth Platformer/Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawn1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.transform.position = respawn1.transform.position;
+        }
+    }
+}
