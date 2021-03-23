95ea8312b6422b2fef5bb16d0203c85e8e3ad41f
imfizh <43548110+imfizh@users.noreply.github.com>
2021-03-23 19:23:02 +0000

    added bridge sprite and level switch
    added bridge sprite to the game and a collider to switch the level

---
+++ 2D Depth Platformer/Assets/Scripts/Change.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        levelLoader load = FindObjectOfType<levelLoader>();
+        //levelLoader load = new levelLoader();
+        load.LoadLevel();
+    }
+}
