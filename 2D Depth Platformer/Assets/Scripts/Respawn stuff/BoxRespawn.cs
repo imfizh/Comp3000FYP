7c0ac20ff8033ad21438891116726b7525e8df3b
imfizh <43548110+imfizh@users.noreply.github.com>
2021-04-01 12:26:43 +0100

    added respawn to the box
    added a respawn to the box so you can no longer softlock the game

---
+++ 2D Depth Platformer/Assets/Scripts/Respawn stuff/BoxRespawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRespawn : MonoBehaviour
+{
+    public Transform location;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "box")
+        {
+            collision.gameObject.transform.position = location.transform.position;
+            collision.gameObject.transform.rotation = location.transform.rotation;
+        }
+    }
+}
