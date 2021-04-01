7d810f34fda2a80663880b765ac58b20427bb8e9
imfizh <43548110+imfizh@users.noreply.github.com>
2021-04-01 15:17:09 +0100

    added bridge ending
    added the bridge ending for the first level

---
+++ 2D Depth Platformer/Assets/Scripts/BridgeTriggerScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTriggerScript : MonoBehaviour
+{
+    float time = 1f;
+    [SerializeField] private GameObject bridgeFull;
+    [SerializeField] private GameObject bridgeBroke;
+    [SerializeField] private GameObject piece1;
+    [SerializeField] private GameObject piece2;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        GameObject.Find("mc").GetComponent<PlayerMovement>().speed = 0;
+        StartCoroutine(Load(1));
+    }
+    IEnumerator Load(int index)
+    {
+        yield return new WaitForSeconds(time);
+        bridgeBroke.SetActive(true);
+        bridgeFull.SetActive(false);
+        piece1.SetActive(true);
+        piece2.SetActive(true);
+    }
+}
