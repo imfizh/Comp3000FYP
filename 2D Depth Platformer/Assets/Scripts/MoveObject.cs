54acb71b2ede73a5ad934330759205965f578193
imfizh <43548110+imfizh@users.noreply.github.com>
2021-04-06 20:47:25 +0100

    added push sound to the box

--- 2D Depth Platformer/Assets/Scripts/MoveObject.cs
+++ 2D Depth Platformer/Assets/Scripts/MoveObject.cs
@@ -9,10 +9,16 @@ public class MoveObject : MonoBehaviour
     public Rigidbody2D rb;
     private bool interact = false;
     private bool stopInteract = false;
+    AudioSource push;
+    private GameObject player;
+    private bool test;
+    private bool stopped;
+    private bool pressed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        push = GameObject.Find("box").GetComponent<AudioSource>();
+        player = GameObject.Find("mc");
     }
 
     // Update is called once per frame
@@ -20,8 +26,25 @@ public class MoveObject : MonoBehaviour
     {
         if (interact == true && Input.GetKeyDown(KeyCode.E))
         {
+            test = true;
             Interaction();
         }
+        if (test == true && player.GetComponent<PlayerMovement>().moveInput != 0)
+        {
+            push.Play();
+            test = false;
+        }
+        if (test == false && player.GetComponent<PlayerMovement>().moveInput == 0)
+        {
+            push.Stop();
+            stopped = true;
+        }
+        if (test == false && interact == true && stopped == true && pressed == true)
+        {
+            test = true;
+            stopped = false;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -43,14 +66,17 @@ public class MoveObject : MonoBehaviour
                 rb.mass = 2000;
                 stopInteract = false;
             }
-            
+            push.Stop();
         }
+        test = false;
+        pressed = false;
     }
 
     public void Interaction()
     {
         uiObject.SetActive(false);
         rb.mass = 20;
+        pressed = true;
         stopInteract = true;
     }
 }
