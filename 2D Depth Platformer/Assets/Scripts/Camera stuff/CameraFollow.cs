5d89aef63a68dc3d7022c1384369ca73fc954e9b
imfizh <43548110+imfizh@users.noreply.github.com>
2021-04-13 17:34:08 +0100

    cleaning up and added lab background
    cleaned up the scenes a bit and added in the lab background to the city level

--- 2D Depth Platformer/Assets/Scripts/Camera stuff/CameraFollow.cs
+++ 2D Depth Platformer/Assets/Scripts/Camera stuff/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    private Vector3 offset = new Vector3(0,0,-28);
+    private Vector3 offset = new Vector3(0,1.5f,-28);
     public float followSpeed = 10f;
     public float xMin = 0f;
     Vector3 velocity = Vector3.zero;
