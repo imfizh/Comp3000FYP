dcfdc46223b71f88e8c7e5ec2f36d157e4e2d09f
imfizh <43548110+imfizh@users.noreply.github.com>
2021-04-19 22:26:50 +0100

    finished door and key
    added dialogue text to door

--- 2D Depth Platformer/Assets/Scripts/Door observer scripts/DoorTrigger.cs
+++ 2D Depth Platformer/Assets/Scripts/Door observer scripts/DoorTrigger.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     public int id;
+    public Dialogue dialogue;
+    public bool isDialogue = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DoorEvents.current.DoorTriggerEnter(id);
+        if (this.GetComponentInChildren<DoorController>() != null)
+        {
+            if (this.GetComponentInChildren<DoorController>().id != id)
+            {
+                TriggerDialogue();
+            }
+        }
+        
+        //TriggerDialogue();
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FindObjectOfType<DiaglogueManager>().EndDialogue();
+        isDialogue = false;
+    }
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DiaglogueManager>().StartDialogue(dialogue);
+        isDialogue = true;
+    }
+    public void Update()
+    {
+        if (isDialogue == true && Input.GetKeyDown(KeyCode.Return))
+        {
+            FindObjectOfType<DiaglogueManager>().DisplayNextSentence();
+        }
     }
 }
